d36c118e6adecbc49bcc010e89ea29c78e664f7c
Eben <73199056+eben92@users.noreply.github.com>
2024-09-23 19:31:20 +0000

    Update setup.sh

--- public/setup.sh
+++ public/setup.sh
@@ -23,7 +23,7 @@ if [ -z "$TAG" ]; then
 	if [ "$UBUNTU_VERSION" == "Ubuntu 22.04 LTS" ]; then
 		# This machine is running Ubuntu 22.04, which is supported by
 		# Mail-in-a-Box versions 60 and later.
-		TAG=v0.1
+		TAG=v1
 	elif [ "$UBUNTU_VERSION" == "Ubuntu 18.04 LTS" ]; then
 		# This machine is running Ubuntu 18.04, which is supported by
 		# Mail-in-a-Box versions 0.40 through 5x.
@@ -31,13 +31,13 @@ if [ -z "$TAG" ]; then
 		echo "Please immediately begin to migrate your data to"
 		echo "a new machine running Ubuntu 22.04. See:"
 		echo "https://mailinabox.email/maintenance.html#upgrade"
-		TAG=v0.1
+		TAG=v1
 	elif [ "$UBUNTU_VERSION" == "Ubuntu 14.04 LTS" ]; then
 		# This machine is running Ubuntu 14.04, which is supported by
 		# Mail-in-a-Box versions 1 through v0.30.
 		echo "Ubuntu 14.04 is no longer supported."
 		echo "The last version of Mail-in-a-Box supporting Ubuntu 14.04 will be installed."
-		TAG=v0.1
+		TAG=v1
 	else
 		echo "This script may be used only on a machine running Ubuntu 14.04, 18.04, or 22.04."
 		exit 1
@@ -50,58 +50,4 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-# Check if token.txt exists
-if [ ! -f "$HOME/token.txt" ]; then
-    echo "token.txt not found!"
-    exit 1
-fi
-
-# Read the GitHub access token from the file
-read -r GITHUB_TOKEN < "$HOME/token.txt"
-
-# Check if the token is empty
-if [ -z "$GITHUB_TOKEN" ]; then
-    echo "GitHub token is empty!"
-    exit 1
-fi
-
-# Clone the The-Box-Oven repository if it doesn't exist.
-if [ ! -d "$HOME/theboxoven" ]; then
-	if [ ! -f /usr/bin/git ]; then
-		echo "Installing git . . ."
-		apt-get -q -q update
-		DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
-		echo
-	fi
-
-	if [ "$SOURCE" == "" ]; then
-		SOURCE="https://$GITHUB_TOKEN:@github.com/eben92/the-box-oven"
-	fi
-
-	echo "Downloading The-Box-Oven $TAG. . ."
-
-	git clone \
-		-b "$TAG" --depth 1 \
-		"$SOURCE" \
-		"$HOME/theboxoven" \
-		< /dev/null 2> /dev/null
-
-	echo
-fi
-
-# Change directory to it.
-cd "$HOME/theboxoven" || exit
-
-# Update it.
-if [ "$TAG" != "$(git describe --always)" ]; then
-	echo "Updating The-Box-Oven to $TAG . . ."
-	git fetch --depth 1 --force --prune origin tag "$TAG"
-	if ! git checkout -q "$TAG"; then
-		echo "Update failed. Did you modify something in $PWD?"
-		exit 1
-	fi
-	echo
-fi
-
-# Start setup script.
-setup/start.sh
+echo "$TAG"
